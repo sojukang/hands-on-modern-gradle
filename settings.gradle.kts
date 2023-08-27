65adc07059f75679f68765b797c3c5235dd265a7
sojukang <kangsburg@gmail.com>
2023-08-27 17:50:30 +0900

    add include modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,28 +1,3 @@
-pluginManagement {
-    repositories.gradlePluginPortal()
-    // repositories.mavenCentral()
-    // repositories.google()
-    // repositories.maven("https://my.location/repo") {
-    //     credentials.username = "..."
-    //     credentials.password = "..."
-    // }
-
-    // includeBuild("gradle/plugins")
-}
-
-dependencyResolutionManagement {
-    repositories.mavenCentral()
-    // repositories.google()
-    // repositories.maven("https://my.location/repo") {
-    //     credentials.username = "..."
-    //     credentials.password = "..."
-    // }
-
-    // includeBuild("../my-other-project")
-}
-
-rootProject.name = "my-project"
-
 include("app")
 include("business-logic")
 include("data-model")
